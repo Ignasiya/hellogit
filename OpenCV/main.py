214685c3c1e89e2fbf8137d4b591e074a69eacc7
Ignasiya <ignasiya93@gmail.com>
2023-04-04 18:06:34 +0300

    Bootcamp

---
+++ OpenCV/main.py
@@ -0,0 +1,10 @@
+import cv2
+
+img = cv2.imread('C:/Users/Василий/Documents/practicecode/GBlearn/OpenCV/test.jpg')
+print(img.shape)
+
+img = cv2.resize(img, (500, 500))
+
+cv2.imshow('Result', img)
+
+cv2.waitKey(0) 
